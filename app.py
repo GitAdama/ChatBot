9e60d193249374ac8fdd1e78299c3c64f6e3d035
GitAdama <58881131+GitAdama@users.noreply.github.com>
2021-12-17 00:57:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -103,6 +103,7 @@ async def messages(req: Request) -> Response:
 def init_func(argv):
     APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
     APP.router.add_post("/api/messages", messages)
+    return
 
 if __name__ == "__main__":
     APP = init_func(None)
